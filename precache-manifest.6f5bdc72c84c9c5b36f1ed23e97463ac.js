2cb8b561ff482ebe230a1570d16a171facd23792
Mike Thomas <mikepthomas@outlook.com>
2019-05-05 21:18:30 +0100

    Updates

---
+++ precache-manifest.6f5bdc72c84c9c5b36f1ed23e97463ac.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5551d6b391e43a308e20947d25db648c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb1c2257f1bf9f8f8e2a",
+    "url": "/static/css/main.f7fa4766.chunk.css"
+  },
+  {
+    "revision": "b0e736e706ed1a764ae9",
+    "url": "/static/js/2.ce57a30c.chunk.js"
+  },
+  {
+    "revision": "cb1c2257f1bf9f8f8e2a",
+    "url": "/static/js/main.9281bb46.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5387b1227d7c49d1df773db89b737676",
+    "url": "/static/media/banner-md.5387b122.jpg"
+  },
+  {
+    "revision": "0aea97a48c19ad3807b07d51dd0df84d",
+    "url": "/static/media/banner-sm.0aea97a4.jpg"
+  },
+  {
+    "revision": "da576c48666bb1cfeecc614177ba3ce1",
+    "url": "/static/media/banner.da576c48.jpg"
+  },
+  {
+    "revision": "7d24898ea7ef19dcdeb0d6d9b9c512b1",
+    "url": "/static/media/guitar-hero.7d24898e.jpg"
+  },
+  {
+    "revision": "0f4bf3f76d4c5e3858b0853c01abff5c",
+    "url": "/static/media/guitar.0f4bf3f7.md"
+  },
+  {
+    "revision": "23ca3e500e1f690a9a2e5832996fabd7",
+    "url": "/static/media/openrc-hero.23ca3e50.jpg"
+  },
+  {
+    "revision": "da565e81d199a3f9a86754422e1acb6e",
+    "url": "/static/media/openrc.da565e81.md"
+  },
+  {
+    "revision": "28988676657cfacf237c1530b4dca01b",
+    "url": "/static/media/printed-items.28988676.jpg"
+  },
+  {
+    "revision": "2b63fd9d29fef8357357b78e69976d16",
+    "url": "/static/media/printer-hero.2b63fd9d.jpg"
+  },
+  {
+    "revision": "f752919996933ae78f5c830560d5e01f",
+    "url": "/static/media/printer.f7529199.md"
+  }
+]);
